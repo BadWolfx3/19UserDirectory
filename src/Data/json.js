8e9eff96a5c40878e9fb8a0ab98615c7ac932d87
Kelsey Busser <busserk@gmail.com>
2021-02-04 19:18:47 -0500

    changed typo

---
+++ src/Data/json.js
@@ -0,0 +1,28 @@
+const persons = [
+    {
+        userId: 1,
+        image: "",
+        name: { first: "Kris", last: "Jacobs" },
+        phone: "5122222222",
+        email: "krish.lee@learningcontainer.com",
+        dob: "05-23-1987"
+    },
+    {
+        userId: 2,
+        image: "",
+        name: { first: "Lucas", last: "Morris" },
+        phone: "123456",
+        email: "racks.jacson@learningcontainer.com",
+        dob: "07-23-1990"
+    },
+    {
+        userId: 3,
+        image: "",
+        name: { first: "Sandy", last: "Ruedell" },
+        phone: "2354430",
+        email: "joe.jacson@learningcontainer.com",
+        dob: "07-23-2000"
+    }
+]
+
+export default persons;
