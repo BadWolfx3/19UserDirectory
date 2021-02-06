8a0c0df63bd9969f7c69d82901500969de4f9541
Kelsey Busser <busserk@gmail.com>
2021-02-06 10:54:09 -0500

    tried to deploy

---
+++ build/precache-manifest.668f9ae530fb17b9c563311ae065c5d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc8a4f723031dad5e703b1ebf15c83a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f36af79b67cde8c8071",
+    "url": "/static/css/main.779d2cf3.chunk.css"
+  },
+  {
+    "revision": "f88807788fc0d58c4233",
+    "url": "/static/js/2.1a8337a4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1a8337a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f36af79b67cde8c8071",
+    "url": "/static/js/main.5bca7a61.chunk.js"
+  },
+  {
+    "revision": "fbca17fa475678276fca",
+    "url": "/static/js/runtime-main.7447f49c.js"
+  }
+]);
